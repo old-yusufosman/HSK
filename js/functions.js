d878fe04b168a980c230cfa9b8e7aa90347360cf
Yusuf Osman <yusuf.o@gmail.com>
2016-05-01 15:49:11 +0100

    ScrollMagic introduced

--- js/functions.js
+++ js/functions.js
@@ -1,17 +1,26 @@
 var pContainerHeight = $('.banner').height();
+var controller = new ScrollMagic.Controller();
 
-$(window).scroll(function(){
+$(window).scroll(function () {
 
-  var wScroll = $(this).scrollTop();
+    var wScroll = $(this).scrollTop();
 
-  if (wScroll <= pContainerHeight) {
+    if (wScroll <= pContainerHeight) {
 
-    $('.logo').css({
-      'transform' : 'translate(0px, '+ wScroll /3.9 +'%)'
-    });
+        $('.logo').css({
+            'transform': 'translate(0px, ' + wScroll / 3.9 + '%)'
+        });
 
-    $('#bannerColourOverlay').css('opacity', wScroll/$('#bannerColourOverlay').height()/3 + 0.5);
+        $('#bannerColourOverlay').css('opacity', wScroll / $('#bannerColourOverlay').height() / 3 + 0.5);
+        $('.blurred-image').css('opacity', wScroll / $('#bannerColourOverlay').height() / 3 + 0.5);
 
-  }
+    }
 
 });
+
+var scene = new ScrollMagic.Scene({
+    triggerElement: "#trigger1"
+})
+    .setTween("#animate1", 0.5, {opacity: 0.5, scale: 2.5}) // trigger a TweenMax.to tween
+    .addIndicators({name: "1 (duration: 0)"}) // add indicators (requires plugin)
+    .addTo(controller);
