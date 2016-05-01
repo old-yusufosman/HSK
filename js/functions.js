712585a04aac088c3e6b171b08fa3d02d44c8889
Yusuf Osman <yusuf.o@gmail.com>
2016-05-01 18:01:04 +0100

    Basic cherry animation

--- js/functions.js
+++ js/functions.js
@@ -24,3 +24,22 @@ var scene = new ScrollMagic.Scene({
     .setTween("#how", 0.5, {opacity: 1.0, top: 0}) // trigger a TweenMax.to tween
     .addIndicators({name: "1 (duration: 0)"}) // add indicators (requires plugin)
     .addTo(controller);
+
+var scene = new ScrollMagic.Scene({
+    triggerElement: "#trigger2"
+})
+    .setTween("#follow", 0.5, {opacity: 1.0, top: 0}) // trigger a TweenMax.to tween
+    .addIndicators({name: "1 (duration: 0)"}) // add indicators (requires plugin)
+    .addTo(controller);
+
+var scene = new ScrollMagic.Scene({
+    triggerElement: "#show-cherry-trigger"
+})
+    .setTween("#cherry", 0.5, {opacity: 1.0, top: 0}) // trigger a TweenMax.to tween
+    .addIndicators({name: "1 (duration: 0)"}) // add indicators (requires plugin)
+    .addTo(controller);
+
+var scene = new ScrollMagic.Scene({triggerElement: "#cherry-move-trigger", duration: 1300})
+    .setPin("#pin1")
+    .addIndicators({name: "1 (duration: 1300)"}) // add indicators (requires plugin)
+    .addTo(controller);
